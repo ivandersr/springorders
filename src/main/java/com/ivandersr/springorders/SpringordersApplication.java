09c94d45053cf36e7dc8d5a011f85e0723a4344f
Ivander <ivandersalvadorruiz@gmail.com>
2024-01-24 10:06:04 -0300

    first commit

---
+++ src/main/java/com/ivandersr/springorders/SpringordersApplication.java
@@ -0,0 +1,13 @@
+package com.ivandersr.springorders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringordersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringordersApplication.class, args);
+	}
+
+}
